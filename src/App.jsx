b60f0008b6b221fd0b0cc129153819264c042ec9
VINÍCIUS SOARES FRANÇA <vinicius_s_franca@estudante.sesisenai.org.br>
2024-02-27 09:02:22 -0300

    Tailwind-CSS

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function App() {
+
+  return (
+    <div className='flex justify-center items-center flex-wrap gap-8 min-h-screen bg-gradient-to-r from-blue-950 to-slate-800'>
+      <div className='h-32 w-60 bg-slate-500'></div>
+      <div className='h-32 w-60 bg-slate-500'></div>
+      <div className='h-32 w-60 bg-slate-500'></div>
+      <div className='h-32 w-60 bg-slate-500'></div>
+      <div className='h-32 w-60 bg-slate-500'></div>
+      <div className='h-32 w-60 bg-slate-500'></div>
+      <div className='h-32 w-60 bg-slate-500'></div>
+      <div className='h-32 w-60 bg-slate-500'></div>
+      <div className='h-32 w-60 bg-slate-500'></div>
+      <div className='h-32 w-60 bg-slate-500'></div>
+    </div>
+  )
+}
+
+export default App
